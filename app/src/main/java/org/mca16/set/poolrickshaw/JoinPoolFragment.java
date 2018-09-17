97e78b259c0cfffa99bf27050e11f144d3e06b80
omkarborkar <omkarborkar95@gmail.com>
2018-09-17 12:48:29 +0530

    onSearchPressed method created.

--- app/src/main/java/org/mca16/set/poolrickshaw/JoinPoolFragment.java
+++ app/src/main/java/org/mca16/set/poolrickshaw/JoinPoolFragment.java
@@ -4,19 +4,32 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import co.chatsdk.core.dao.Thread;
 import co.chatsdk.core.dao.User;
+import co.chatsdk.core.session.ChatSDK;
 import co.chatsdk.ui.main.BaseFragment;
 import co.chatsdk.ui.profile.ProfileFragment;
+import java.util.*;
+
 
 public class JoinPoolFragment extends BaseFragment {
 
     @Override
     public void clearData() {
 
+    }
+
+    public void onSearchPressed(){
+        for( Thread thread : ChatSDK.thread().getThreads(1)){
+
+
+            Log.d("Thread","--------Name:"+thread.getName()+"-----EntityId:"+thread.getEntityID()+"-------");
+        }
     }
 
     @Override
